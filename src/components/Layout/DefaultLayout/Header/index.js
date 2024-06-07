18cca6b3f97de70777c4efc4c62878afec4f5cbf
fightforfreedom2k3 <nhhoang15122003@gmail.com>
2024-06-07 11:11:00 +0700

    update account-navigation

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -1,41 +1,52 @@
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
-import { Box, Button, IconButton, Stack } from '@mui/material';
+import { Menu, MenuItem, Box, Button, IconButton, Stack } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
-import PersonIcon from '@mui/icons-material/Person';
+import React, { useState } from 'react';
+import AccountMenu from '../../../AccountMenu/AccountMenu';
 
 const cx = classNames.bind(styles);
 
 function Header() {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
         <header className={cx('wrapper')}>
-            <Stack p={1.5} direction={'row'} justifyContent={'space-between'}>
+            <Stack p={1.2} direction={'row'} justifyContent={'space-between'}>
                 {/* Thanh Navigation */}
                 <Stack direction={'row'} spacing={1}>
                     <IconButton>
-                        <MenuIcon></MenuIcon>
+                        <MenuIcon fontSize="large"></MenuIcon>
                     </IconButton>
 
                     <IconButton>
-                        <SearchIcon></SearchIcon>
+                        <SearchIcon fontSize="large"></SearchIcon>
                     </IconButton>
                 </Stack>
+
                 {/* Logo */}
-                <div className={cx('header-logo')}>
+                <Stack>
                     <Box
                         component={'img'}
                         src={'https://i.pinimg.com/736x/26/91/f2/2691f2fa1a0f078f5f274edf7fea6763.jpg'}
-                        width={100}
-                        height={50}
+                        width={170}
+                        height={52}
                     ></Box>
-                </div>
+                </Stack>
+
                 {/* Account */}
-                <div className={cx('account-item')}>
-                    <Button>
-                        <PersonIcon></PersonIcon>
-                    </Button>
-                </div>
+                <Stack>
+                    <AccountMenu />
+                </Stack>
             </Stack>
         </header>
     );
